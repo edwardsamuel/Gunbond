0b36962f7c3abafab0f82f1d1cf713267fac6644
Edward Samuel <edward_samuel_esp@yahoo.com>
2013-05-13 08:03:32 +0700

    Tracker Command

--- Sister-2/Gunbond-Tracker/Program.cs
+++ Sister-2/Gunbond-Tracker/Program.cs
@@ -31,15 +31,67 @@ namespace Gunbond_Tracker
             {
                 String input = Console.ReadLine();
                 String[] parsed = input.Split(' ');
-
-                if (parsed[0].ToLower().Equals("shutdown"))
+                if (parsed[0].ToLower().Equals("max_peer"))
+                {
+                    int max_peer;
+                    if ((parsed.Length == 2) && (Int32.TryParse(parsed[1], out max_peer)))
+                    {
+                        tracker.Configuration.MaxPeer = max_peer;
+                        Console.WriteLine("Max peer is set to " + max_peer + ".");
+                        tracker.Configuration.Print();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parameter of max_peer is wrong.");
+                    }
+                }
+                else if (parsed[0].ToLower().Equals("max_room"))
+                {
+                    int max_room;
+                    if ((parsed.Length == 2) && (Int32.TryParse(parsed[1], out max_room)))
+                    {
+                        tracker.Configuration.MaxRoom = max_room;
+                        Console.WriteLine("Max room is set to " + max_room + ".");
+                        tracker.Configuration.Print();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parameter of max_room is wrong.");
+                    }
+                }
+                else if (parsed[0].ToLower().Equals("log"))
+                {
+                    if ((parsed.Length == 2) && ("on".Equals(parsed[1].ToLower())))
+                    {
+                        tracker.Configuration.Log = true;
+                        Logger.Active = tracker.Configuration.Log;
+                        Console.WriteLine("Log is on.");
+                        tracker.Configuration.Print();
+                    }
+                    else if ((parsed.Length == 2) && ("off".Equals(parsed[1].ToLower())))
+                    {
+                        tracker.Configuration.Log = false;
+                        Logger.Active = tracker.Configuration.Log;
+                        Console.WriteLine("Log is off.");
+                        tracker.Configuration.Print();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parameter of log is wrong.");
+                    }
+                }
+                else if (parsed[0].ToLower().Equals("shutdown"))
                 {
                     check = false;
+                }
+                else
+                {
+                    Console.WriteLine("Command unrecognized.");
                 }
             }
 
             tracker.Configuration.SaveData("config.xml");
-            Console.WriteLine("GunBond Tracker is going to turn off.");
+            Console.WriteLine("Gunbond Tracker is going to turn off.");
         }
     }
 }
